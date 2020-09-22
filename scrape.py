d50e5cb0ad66a2da3ca42e58d388b2d5aa38f7b8
Spencer Ye <sye348@student.fuhsd.org>
2020-09-21 22:46:32 -0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,8 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 def work():
 	URL = 'https://www.athletic.net/TrackAndField/SchoolRecords.aspx?SchoolID=1096'
 	page = requests.get(URL)
 	print (type(page.content))
 	file = open("output.html", "w")
-	file.write(str(page.content))
+	soup = BeautifulSoup(page.content, 'html.parser')
+	file.write(str(soup))
