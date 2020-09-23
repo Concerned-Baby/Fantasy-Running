bf6d9b36bb6b086063eb71f08ea406cff39f7122
Spencer Ye <sye348@student.fuhsd.org>
2020-09-22 21:05:18 -0700

    try to database

---
+++ server/database.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+def start():
+	db = mysql.connector.connect(
+		host="localhost",
+		user="spinkr",
+		password="hi"
+		)
+	print(db)
--- server/main.py
+++ server/main.py
@@ -1,7 +1,8 @@
-
+import database
 
 def main():
 	print("started")
+	database.start()
 
 if __name__ == "__main__":
 	main()
